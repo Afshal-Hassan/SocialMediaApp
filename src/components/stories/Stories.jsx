8797f9eab9d58392da7fafb53d903e86ee29af01
Afshal Hassan <afshal.hassan@venturedive.com>
2023-01-31 05:47:10 +0500

    Stories component added

--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -6,91 +6,92 @@ import { useRef } from 'react';
 import "./Stories.css";
 
 
-function Stories() {
-    const [isModalOpen , setModalOpen] = useState(false);
+const stories = [
+    {
+        id: 1,
+        url: "http://localhost:5000/videos/c7b01bab-3eb3-42aa-ba87-79ae4997d2de.mp4"
+    },
+    {
+        id: 2,
+        url: "http://localhost:5000/videos/d2506aa3-148a-4c42-8dfa-fbf11b6e8893.mp4"
+    }
+]
 
-    let videoRef = useRef(null);
+
+function Stories() {
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [modalInfo, setModalInfo] = useState(null);
+    const videoRef = useRef();
+
 
     const playVideo = () => {
         videoRef.current.play();
     }
     const pauseVideo = () => {
         videoRef.current.pause();
-        videoRef=null;
     }
-    const openStoriesModal = () => {
+    const openStoriesModal = (story) => {
+        setModalInfo(story);
         setModalOpen(true);
         setTimeout(() => {
             playVideo();
-        },500);
+        }, 500);
+    }
+
+    const ModalContent = (props) => {
+        console.log(props.story);
+        if (modalInfo != null) {
+            return (
+                <Modal open={isModalOpen} footer={null} closable={false} className="ant-modal-content">
+                    <div className='modal-box' onClick={closeStoriesModal}>
+                        <Box>
+                            <video style={{ width: "100%", height: "100%", boxSizing: "border-box", borderRadius: 8 }} src={props.story.url} ref={videoRef} controls />
+                        </Box>
+                    </div>
+                </Modal>
+            );
+        }
+
     }
 
     const closeStoriesModal = () => {
-        
         setTimeout(() => {
             setModalOpen(false);
-        },300);
+        }, 300);
 
-        setTimeout(() => {
-            pauseVideo();
-        },550);
-               
     }
 
-   console.log(videoRef);
-    useEffect(()=>{
-
-    },[]);
 
     return (
         <div
-        style={{display:"flex" , flexDirection:"row" , alignItems:"center",width:"100%",marginTop: 20,}}
+            style={{ display: "flex", flexDirection: "row", alignItems: "center", width: "100%", marginTop: 20 }}
         >
-            <div
-                style={{
-                    width: "10em",
-                    height: "15em",
-                    borderRadius: 8,
-                    boxShadow: "3px 3px 5px 1.5px lightgray",
-                    
-                    marginLeft:10,
-                    marginRight:25,
-                    position:"relative"
-                }}
-                className="video-component"
-                onClick={openStoriesModal}
-            >
-                <video style={{ width: "100%", height: "100%", boxSizing: "border-box", borderRadius: 8 }} className="video" src={"http://localhost:5000/videos/c7b01bab-3eb3-42aa-ba87-79ae4997d2de.mp4"} autoPlay muted loop />
-            <span style={{position:"absolute",left:"37%",top:"87%",color:"whitesmoke",fontWeight:600,fontSize:13.5}} className="video-title">Audi Lifestyle</span>
-            <Modal open={isModalOpen} footer={null} closable={false}  className="ant-modal-content">
-                    <div className='modal-box' onClick={closeStoriesModal}>
-                   <Box>
-                    <video style={{ width: "100%", height: "100%", boxSizing: "border-box", borderRadius: 8 }}  src={"http://localhost:5000/videos/c7b01bab-3eb3-42aa-ba87-79ae4997d2de.mp4"} ref={videoRef} controls  />
-                    </Box>
-                    </div>
-                </Modal>
-            </div>
-            <div
-                style={{
-                    width: "10em",
-                    height: "15em",
-                    borderRadius: 8,
-                    boxShadow: "3px 3px 5px 1.5px lightgray",
-                    position:"relative"
-                }}
-                className="video-component"
-                onClick={openStoriesModal}
-            >
-                <video style={{ width: "100%", height: "100%", boxSizing: "border-box", borderRadius: 8 }} className="video" src={"http://localhost:5000/videos/c7b01bab-3eb3-42aa-ba87-79ae4997d2de.mp4"} autoPlay muted loop />
-                <span style={{position:"absolute",left:"37%",top:"87%",color:"whitesmoke",fontWeight:600,fontSize:13.5}} className="video-title">Audi Lifestyle</span>
-                <Modal open={isModalOpen} footer={null} closable={false}  className="ant-modal-content">
-                    <div className='modal-box' onClick={closeStoriesModal}>
-                   <Box>
-                    <video style={{ width: "100%", height: "100%", boxSizing: "border-box", borderRadius: 8 }}  src={"http://localhost:5000/videos/c7b01bab-3eb3-42aa-ba87-79ae4997d2de.mp4"} ref={videoRef} controls  />
-                    </Box>
-                    </div>
-                </Modal>
-            </div>
+            
+
+            {
+                stories.map(story => {
+                    return (
+                        <div
+                            style={{
+                                width: "10em",
+                                height: "15em",
+                                borderRadius: 8,
+                                boxShadow: "3px 3px 5px 1.5px lightgray",
+                                marginLeft: 10,
+                                marginRight: 25,
+                                position: "relative",
+                            }}
+                            className="video-component"
+                            onClick={() => openStoriesModal(story)}
+                            key={story.id}
+                        >
+                            <video style={{ width: "100%", height: "100%", boxSizing: "border-box", borderRadius: 8 }} className="video" src={story.url} autoPlay muted loop />
+                            <span style={{ position: "absolute", left: "37%", top: "87%", color: "whitesmoke", fontWeight: 600, fontSize: 13.5 }} className="video-title">Audi Lifestyle</span>
+                        </div>
+                    )
+                })
+            }
+            {isModalOpen && <ModalContent story={modalInfo} />}
         </div>
     )
 }
